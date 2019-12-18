e60f3e7ffb6426a7dbd42b57ba3916d7e54a855b
uma <umathan13@gmail.com>
2019-12-18 14:14:19 -0500

    correction in DbSet

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using StarChart.Models;
-
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObjects;
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
-            public DbSet<CelestialObject> CelestialObjects;
+
 
         }
     }
